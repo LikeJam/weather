6b21512e0399e51af3dc155424b60e9d64529745
jamshidsaidmurodov <jamshid.sadmurodov@danads.se>
2024-07-26 05:23:43 +0500

    Install and configure vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import vuetify from './vuetify.js';
 
 createInertiaApp({
     resolve: name => {
@@ -10,6 +11,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .mount(el)
     },
 })
